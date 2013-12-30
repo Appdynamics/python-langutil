a8ba51be19971fe520727214f4c596a826fcf76e
Andrew Udvare <audvare@gmail.com>
2013-12-30 14:19:30 -0800

    Use phpserialize module's functions first; leave fallback in Python for serialize()

--- langutil/php.py
+++ langutil/php.py
@@ -132,6 +132,12 @@ def generate_array(list_or_array, indent=2, last_level=0, end=';'):
     return '\n'.join(parts)
 
 def serialize(mixed_value):
+    try:
+        from phpserialize import serialize as s
+        return s(mixed_value)
+    except ImportError:
+        pass
+
     """Based on http://phpjs.org/functions/unserialize/"""
     ktype = vals = ''
     count = 0
@@ -169,86 +175,5 @@ def serialize(mixed_value):
     return 'a:%d:{%s}' % (length, ''.join(serialized_set),)
 
 def unserialize(str):
-    php_type_s = str[0]
-
-    if str == 'N;':
-        return None
-
-    if php_type_s not in ('b', 'i', 'd', 's', 'a'):
-        raise PHPSerializationException('Value does not look like a serialised PHP value')
-
-    if php_type_s == 'b':
-        if str[2:-1] == '1':
-            return True
-
-        return False
-    elif php_type_s == 'i':
-        return int(str[2:-1])
-    elif php_type_s == 'd':
-        return float(str[2:-1])
-    elif php_type_s == 's':
-        matches = re.match(r'^s\:(?P<length>\d+)\:"(?P<string>[^\"]+)\";', str)
-
-        if matches is None:
-            raise PHPSerializationException('Value of type string could not be unserialised')
-
-        matches = matches.groupdict()
-        length = int(matches['length'])
-
-        if len(matches['string']) != length:
-            message = 'Value of type string does not match length in serialised value (expected %d, got %d)' % (length, len(str))
-            #raise PHPSerializationException('Value of type string does not match length in serialised value (expected %d, got %d)' % (length, len(str)))
-            sys.stderr.write(message + '\n')
-
-        return matches['string']
-
-    # Dictionary or list
-    matches = re.match('^a\:(?P<length>\d+)\:', str)
-
-    if matches is None:
-        raise PHPSerializationException('Not a valid PHP serialised value')
-
-    list_or_dict_length = int(matches.groupdict()['length'])
-    is_list = False
-    data = str[5:].split(';')[:-1]
-    data = zip(data[0::2], data[1::2])
-    i_count = 0
-
-    for (key, value) in data:
-        if key[0] == 'i':
-            i_count += 1
-
-    is_list = i_count == list_or_dict_length
-
-    # Base case (no list or dict inside)
-    if 'a:' not in str[2:]:
-        if is_list:
-            data = str[5:-1].split(';')[:-1]
-            data = zip(data[0::2], data[1::2])
-            ret = []
-
-            for (i, v) in data:
-                ret.append(unserialize(v + ';'))
-        else:  # Dictionary
-            data = str[5:-1].split(';')[:-1]
-            data = zip(data[0::2], data[1::2])
-            ret = {}
-
-            for (key, value) in data:
-                key = unserialize(key + ';')
-                value = unserialize(value + ';')
-                ret[key] = value
-    else:  # Dictionary or list but has dictionary or list within
-        raise NotImplementedError('Deeply nested arrays are not yet supported')
-
-        array_positions = []
-        array_pos_key = 0
-
-        matches = re.findall(_SERIALIZED_ARRAY_REGEX, str[2:])  # Find all the inner arrays one level deep
-        ret = []
-
-        for array_str in matches:
-            dict_or_list = unserialize(array_str)
-            print(dict_or_list)
-
-    return ret
+    from phpserialize import unserialize as u
+    return u(str)
